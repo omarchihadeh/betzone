a7948305aee27d08adfc8637ee3897191f2269ca
omarchihadeh <omar.chihadeh.1@gmail.com>
2026-05-15 12:26:34 +0300

    Modified builds and outputs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
+  const env = loadEnv(mode, process.cwd(), "");
+
+  let outDir = "build";
+
+  if (env.VITE_APP_TYPE === "BETZONE") {
+    outDir = "build/build-betzone";
+  } else if (env.VITE_APP_TYPE === "PAYMENT") {
+    outDir = "build/build-payment";
+  }
+
   return {
     plugins: [react()],
     server: {
       port: parseInt(env.VITE_PORT) || 5173,
     },
-  }
-})
+    build: {
+      outDir,
+    },
+  };
+});
